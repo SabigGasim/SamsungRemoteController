03e5896a70303c873f3e501fb5930a3f7b374111
unknown <sabgasim@gmail.com>
2024-12-13 05:21:12 +0300

    refactor src/IntervalExecutor.cpp to utilize esp FreeRTOS instead of pthread

--- src/IntervalExecutor.cpp
+++ src/IntervalExecutor.cpp
@@ -2,72 +2,64 @@
 #include <tuple>
 #include <chrono>
 #include <atomic>
-#include <pthread.h>
-#include <thread>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 template <typename... Args>
 class IntervalExecutor {
 public:
     IntervalExecutor(int64_t interval, std::function<void(Args...)> callback)
-        : m_Interval(std::chrono::milliseconds(interval)),
-          m_LastTimeCalled(std::chrono::high_resolution_clock::now()),
+        : m_Interval(interval),
+          m_LastTimeCalled(0),
           m_IsRunning(false),
           m_Callback(std::move(callback)),
-          m_WorkerThread()
-    {
-        pthread_attr_init(&m_ThreadAttr);
-
-        size_t stackSize = 8 * 1024;
-        pthread_attr_setstacksize(&m_ThreadAttr, stackSize);
-    }
+          m_TaskHandle(nullptr) {}
 
     void Execute(Args... args) {
         m_Args = std::make_tuple(std::forward<Args>(args)...);
-        m_LastTimeCalled = std::chrono::high_resolution_clock::now();
+        m_LastTimeCalled = xTaskGetTickCount();
 
         if (m_IsRunning.load()) {
             return;
         }
 
-        if (m_WorkerThread) {
-            pthread_join(m_WorkerThread, nullptr);
+        if (m_TaskHandle != nullptr) {
+            vTaskDelete(m_TaskHandle);
         }
 
-        pthread_create(&m_WorkerThread, &m_ThreadAttr, ThreadFunction, this);
+        m_IsRunning.store(true);
+        xTaskCreate(TaskFunction, "IntervalTask", 2048, this, 1, &m_TaskHandle);
     }
 
     ~IntervalExecutor() {
-        if (m_IsRunning.load() && m_WorkerThread) {
-            pthread_join(m_WorkerThread, nullptr);
+        if (m_IsRunning.load() && m_TaskHandle != nullptr) {
+            vTaskDelete(m_TaskHandle);
         }
-        pthread_attr_destroy(&m_ThreadAttr);
     }
-    
-private:
-    typedef std::chrono::_V2::system_clock::time_point timepoint;
 
+private:
     std::tuple<Args...> m_Args;
-    std::chrono::milliseconds m_Interval;
-    timepoint m_LastTimeCalled;
+    int64_t m_Interval;
+    TickType_t m_LastTimeCalled;
     std::atomic<bool> m_IsRunning;
     std::function<void(Args...)> m_Callback;
-    pthread_t m_WorkerThread;
-    pthread_attr_t m_ThreadAttr;
+    TaskHandle_t m_TaskHandle;
 
-    static void* ThreadFunction(void* arg) {
+    static void TaskFunction(void* arg) {
         IntervalExecutor* self = static_cast<IntervalExecutor*>(arg);
-        self->m_IsRunning.store(true);
 
         while (true) {
-            std::this_thread::sleep_for(self->m_Interval);
-            auto now = std::chrono::high_resolution_clock::now();
-            if (now - self->m_LastTimeCalled >= self->m_Interval) {
+            vTaskDelay(pdMS_TO_TICKS(self->m_Interval));
+
+            TickType_t now = xTaskGetTickCount();
+            if ((now - self->m_LastTimeCalled) >= pdMS_TO_TICKS(self->m_Interval)) {
                 std::apply(self->m_Callback, self->m_Args);
                 break;
             }
         }
 
         self->m_IsRunning.store(false);
-        return nullptr;
-    }    
+        self->m_TaskHandle = nullptr;
+        vTaskDelete(nullptr);
+    }
 };
